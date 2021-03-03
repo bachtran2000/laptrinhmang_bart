73558c12bdb6f8378e73ffd645dbe6d592b4bed6
Trần Cao Minh Bách <trancaominhbach@Trans-MacBook-Pro.local>
2021-03-03 14:33:04 +0700

    RMI tong hieu fix

--- src/RMI_baitap_tong_hieu/Client.java
+++ src/RMI_baitap_tong_hieu/Client.java
@@ -5,32 +5,42 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+
 public class Client {
     public Client() throws Exception {
+        while (true) {
+            menu();
+            int c = new Scanner(System.in).nextInt();
+            int a, b;
 
-        menu();
-        int c = new Scanner(System.in).nextInt();
-
-        System.out.print("Nhap vao so a: ");
-        int a = new Scanner(System.in).nextInt();
-        System.out.print("Nhap vao so b: ");
-        int b = new Scanner(System.in).nextInt();
-        Registry client = LocateRegistry.getRegistry("localhost", 2349);
-        interface_rmi regis = (interface_rmi) client.lookup("Server");
-        switch (c) {
-            case 1:
-                System.out.println("Gia tri phep toan la: a + b =" + regis.tong(a, b));
-                break;
-            case 2:
-                System.out.println("Gia tri phep toan la: a - b = " + regis.hieu(a, b));
+            Registry client = LocateRegistry.getRegistry("localhost", 2349);
+            interface_rmi regis = (interface_rmi) client.lookup("Server");
+            switch (c) {
+                case 1:
+                    System.out.print("Nhap vao so a: ");
+                    a = new Scanner(System.in).nextInt();
+                    System.out.print("Nhap vao so b: ");
+                    b = new Scanner(System.in).nextInt();
+                    System.out.println("Gia tri phep toan la: a + b = " + regis.tong(a, b));
+                    break;
+                case 2:
+                    System.out.print("Nhap vao so a: ");
+                    a = new Scanner(System.in).nextInt();
+                    System.out.print("Nhap vao so b: ");
+                    b = new Scanner(System.in).nextInt();
+                    System.out.println("Gia tri phep toan la: a - b = " + regis.hieu(a, b));
+                default:
+                    System.out.println("Chon sai! Yeu cau chon lai!");
+            }
         }
-
     }
+
 
     public void menu() {
         System.out.println("1. tinh tong");
         System.out.println("2. tinh hieu");
-        System.out.println("Chon: ");
+        System.out.print("Chon: ");
     }
 
     public static void main(String[] args) throws Exception, NotBoundException {
