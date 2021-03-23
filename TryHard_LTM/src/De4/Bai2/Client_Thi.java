481fa1009c7ce375ce24dcd18812ef286f9c8b69
Trần Cao Minh Bách <trancaominhbach@Trans-MacBook-Pro.local>
2021-03-23 22:27:18 +0700

    Try Hard LTM de 4 bai 2

--- TryHard_LTM/src/De4/Bai2/Client_Thi.java
+++ TryHard_LTM/src/De4/Bai2/Client_Thi.java
@@ -1,4 +1,66 @@
 package De4.Bai2;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
 public class Client_Thi {
+    public static void menu(){
+        System.out.println("1. Bat dau thi");
+        System.out.println("2. Xem ket qua thi");
+        System.out.println("3. Thoat");
+        System.out.print("Chon: ");
+    }
+
+    public static void SendBaithi(DataInputStream dis, DataOutputStream dos, int n) throws IOException {
+
+        System.out.println("Bat dau thi: ");
+        //1
+        System.out.println(dis.readUTF());
+        String check = new Scanner(System.in).nextLine();
+        dos.writeUTF(check);
+
+        //2
+        System.out.println(dis.readUTF());
+        check = new Scanner(System.in).nextLine();
+        dos.writeUTF(check);
+
+        //3
+        System.out.println(dis.readUTF());
+        check = new Scanner(System.in).nextLine();
+        dos.writeUTF(check);
+
+        System.out.println(dis.readUTF());
+    }
+    public static void main(String[] args) throws IOException {
+        Socket client = new Socket("127.0.0.1",2349);
+        DataInputStream dis = new DataInputStream(client.getInputStream());
+        DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+        while (true){
+            menu();
+            int n = new Scanner(System.in).nextInt();
+
+            switch (n){
+                case 1:
+                    dos.writeInt(n);
+                    SendBaithi(dis,dos,n);
+                    break;
+                case 2:
+                    dos.writeInt(n);
+                    System.out.println(dis.readUTF());
+                    break;
+                case 3:
+                    dos.writeInt(n);
+                    exit(0);
+            }
+        }
+
+
+    }
+
 }
--- TryHard_LTM/src/De4/Bai2/Server_Thi.java
+++ TryHard_LTM/src/De4/Bai2/Server_Thi.java
@@ -1,4 +1,55 @@
 package De4.Bai2;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static java.lang.System.exit;
+
 public class Server_Thi {
+    public static int Question(DataInputStream dis, DataOutputStream dos) throws IOException {
+        int flag = 0;
+
+        dos.writeUTF("Cau 1: 1+1 = ?");
+        if (dis.readUTF().equals("2"))
+            flag++;
+
+        dos.writeUTF("Cau 2: 2+2 = ?");
+        if (dis.readUTF().equals("4"))
+            flag++;
+
+        dos.writeUTF("Cau 3: 4*4 = ?");
+        if (dis.readUTF().equals("16"))
+            flag++;
+
+        return flag;
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(2349);
+        Socket my_client = server.accept();
+        DataInputStream dis = new DataInputStream(my_client.getInputStream());
+        DataOutputStream dos = new DataOutputStream(my_client.getOutputStream());
+
+        int flag=0;
+        while (true) {
+            int n = dis.readInt();
+            switch (n) {
+                case 1:
+                    flag = Question(dis, dos);
+                    if (flag >= 2)
+                        dos.writeUTF("Chuc mung ban da thi do");
+                    else dos.writeUTF("Tach roi ban oi");
+                    break;
+                case 2:
+                    dos.writeUTF("Ket qua thi cua ban la: " +flag +"/3 cau dung");
+                    break;
+                case 3:
+                    exit(0);
+            }
+        }
+    }
 }
